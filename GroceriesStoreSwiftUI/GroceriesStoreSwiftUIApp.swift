ed1e889ca0101181ae48384894e063c852971bb3
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-03-05 08:21:38 -0400

    Initial Commit

---
+++ GroceriesStoreSwiftUI/GroceriesStoreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesStoreSwiftUIApp.swift
+//  GroceriesStoreSwiftUI
+//
+//  Created by Jackson Cuevas on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesStoreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
